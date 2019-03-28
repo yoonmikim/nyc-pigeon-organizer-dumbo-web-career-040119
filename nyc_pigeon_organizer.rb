765c3f959d9732b9745ed1519baf70880451fd57
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-28 23:32:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ def nyc_pigeon_organizer(data)
   # write your code here!
     data.each_with_object({}) { |(k,h),o| 
       h.each { |v, n|
-    (o[v] ||= []) <<k
+        
+    (o[v] ||= []) << k
       }
     }
 end
