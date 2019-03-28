9f2577d39580adb1abec5a7bc96927fbe1e61c69
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-28 23:21:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,4 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  class Hash
-  def safe_invert
-    self.each_with_object({}){|(k,v),o|(o[v]||=[])<<k}
-  end
-end
+    data.each_with_object({}){|(k,v),o|(o[v]||=[])<<k}
 end
